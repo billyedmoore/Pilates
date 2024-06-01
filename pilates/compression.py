77281723f4a566b53ba0e2cf0c9a6c1b6f6a175c
Billy Moore <billyedmoore@yahoo.com>
2024-06-01 15:04:16 +0100

    Moved the docstring in pilates/compression.py

--- pilates/compression.py
+++ pilates/compression.py
@@ -1,14 +1,14 @@
 import zlib 
 
 
-"""
-Decompress the data using DEFLATE as specified in the PNG specification.
-Aspirationally would like to write my own version of this.
-
-@param data: the data to be decompressed
-@returns: the decompressed data
-    """
 def inflate(data : bytes) -> bytes:
+    """
+    Decompress the data using DEFLATE as specified in the PNG specification.
+    Aspirationally would like to write my own version of this.
+    
+    @param data: the data to be decompressed
+    @returns: the decompressed data
+        """
     decompress = zlib.decompressobj()
     inflated = decompress.decompress(data)
     inflated += decompress.flush()
