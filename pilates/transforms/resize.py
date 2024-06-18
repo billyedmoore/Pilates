048e2d06d4a3e9e9f294314565a84f39851447d6
Billy Moore <billyedmoore@yahoo.com>
2024-06-18 17:07:58 +0100

    Added better documentation to resize class.

--- pilates/transforms/resize.py
+++ pilates/transforms/resize.py
@@ -1,5 +1,6 @@
 import math
 from typing import List
+import logging
 
 from .transform import Transform
 from ..image import Image
@@ -17,6 +18,9 @@ class Resize(Transform):
         """
 
         def list_mul(lst: List[float] | List[int], x: float) -> List[float] | List[int]:
+            """
+            Multiply each element in a list by x. 
+            """
             new_lst = []
             for e in lst:
                 new_lst.append(e*x)
@@ -26,7 +30,7 @@ class Resize(Transform):
         def list_add(lst1: List[float] | List[int], lst2: List[float] | List[int], lst3: List[float] | List[int], lst4: List[float] | List[int]) -> List[int]:
             """
             Add each element of each list. ie lst1[i]+lst2[i]+... Convert the result to an 
-            int 
+            int.
             """
             if not (len(lst1) == len(lst2)) or not (len(lst2) == len(lst3)) or not (len(lst3) == len(lst4)):
                 raise ValueError(
@@ -41,25 +45,27 @@ class Resize(Transform):
         w, h = img.shape
         new_w, new_h = self.new_shape
         resized_pixels = [[[] for _ in range(new_w)] for _ in range(new_h)]
-
+        
+        # get the ratios of new_shape/old_shape
         x_ratio = (w-1)/(new_w-1) if new_w > 1 else 0
         y_ratio = (h-1)/(new_h-1) if new_h > 1 else 0
 
         for new_x in range(new_w):
             for new_y in range(new_h):
+                # get the four closest pixels
                 x1 = min(math.floor(x_ratio * new_x), w-1)
                 y1 = min(math.floor(y_ratio*new_y), h-1)
                 x2 = min(math.ceil(x_ratio * new_x), w-1)
                 y2 = min(math.ceil(y_ratio*new_y), h-1)
-                # print(f"x {new_x} y {new_y} x1 {x1} x2 {x2} y1 {y1} y2 {y2}")
-
-                x_weight = (x_ratio * new_x) - x1
-                y_weight = (y_ratio * new_y) - y1
 
                 a = img.get_pixel(x1, y1)
                 b = img.get_pixel(x2, y1)
                 c = img.get_pixel(x1, y2)
                 d = img.get_pixel(x2, y2)
+                
+                # find the weights 
+                x_weight = (x_ratio * new_x) - x1
+                y_weight = (y_ratio * new_y) - y1
 
                 pixel = list_add(list_mul(a, (1-x_weight)*(1-y_weight)),
                                  list_mul(b, x_weight*(1-y_weight)),
@@ -67,10 +73,9 @@ class Resize(Transform):
                                  list_mul(d, x_weight*y_weight))
 
                 resized_pixels[new_y][new_x] = pixel
-
+        
+        logging.info(f"Resized image from {img.shape} to {self.new_shape}.")
         img.replace_pixels(resized_pixels)
-
-        print("Resized")
 
     @property
     def new_shape(self):
